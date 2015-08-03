0e289dc6aca8896dc0f17d6103aa62be9db5745a
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-08-04 04:24:58 +0900

    見えてきた気がする

--- src/Main.purs
+++ src/Main.purs
@@ -1,5 +1,20 @@
 module Main where
 
-import Control.Monad.Eff.Console (log)
+import Prelude --(Unit())
+import DOM (DOM())
+import Control.Monad.Eff (Eff())
+import Control.Monad.Eff.Console (CONSOLE(), log)
+import Data.DOM.Simple.Document (Document, title)
+import Data.DOM.Simple.Element (getElementsByClassName)
+import Data.DOM.Simple.Window (document, globalWindow)
 
-main = log "Hi"
+main :: forall e. Eff (dom :: DOM, console :: CONSOLE | e) Unit
+main = do
+  doc <- document globalWindow
+  t <- title doc
+  log t
+--  fileDivs <- getElementsByClassName "file" doc
+--  log fileDivs
+
+
+-- getElementsByClassName :: forall eff. String -> b -> Eff (dom :: DOM | eff) (Array HTMLElement)
