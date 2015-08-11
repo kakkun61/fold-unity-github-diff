25a5649daf6bc1dcc5c6e530a3055cb90741f1bd
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-08-11 23:37:00 +0900

    それっぽく畳み込めた
    まだデザインがおかしい
    拡張はできない

--- src/Main.purs
+++ src/Main.purs
@@ -4,30 +4,59 @@ import Prelude
 import DOM
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
-import Data.Array
+import qualified Data.Array as A
 import Data.Foldable
 import Data.Maybe
+import qualified Data.String as S
 import Data.String.Regex
 import Data.DOM.Simple.Document
 import Data.DOM.Simple.Element
-import Data.DOM.Simple.Window
+import Data.DOM.Simple.Window hiding (search)
 
 --main :: forall e. Eff (dom :: DOM, console :: CONSOLE | e) Unit
 main = do
   print "fold Unity GitHub diff"
   doc <- document globalWindow
   fileDivs <- getElementsByClassName "file" doc
-  traverse_ eachFile fileDivs
+  shouldFoldFileDivs <- A.filterM shouldFold fileDivs
+  traverse_ (collapse doc) shouldFoldFileDivs
   where
-    eachFile div = do
+    shouldFold div = do
       titleSpans <- getElementsByClassName "js-selectable-text" div
-      case head titleSpans of
+      case A.head titleSpans of
         Just titleSpan -> do
           title <- getAttribute "title" titleSpan
-          print title
           let pattern = regex "(\\.unity|\\.meta|\\.prefab)$" noFlags
-          print $ test pattern title
+          return $ test pattern title
+        Nothing -> return false
+    collapse doc div = do
+      tbodyId <- ("orig-" ++) <<< (S.drop 5) <$> getAttribute "id" div
+      tables <- getElementsByClassName "diff-table" div
+      case A.head tables of
+        Just table -> do
+          tbodies <- children table
+          case A.head tbodies of
+            Just tbody -> do
+              setAttribute "id" tbodyId tbody
+              setStyleAttr "display" "none" tbody
+            Nothing -> return unit
+          gcTbody <- createGcTbody doc
+          appendChild table gcTbody
         Nothing -> return unit
+      where
+        createGcTbody doc = do
+          tbody <- createElement "tbody" doc
+          setInnerHTML
+            "<tr class=\"file-diff-line gc\">\n\
+            \  <td class=\"diff-line-num expandable-line-num\" colspan=\"2\">\n\
+            \    <span class=\"diff-expander js-expand\" title=\"Expand\" aria-label=\"Expand\">\n\
+            \      <span class=\"octicon octicon-unfold\"></span>\n\
+            \    </span>\n\
+            \  </td>\n\
+            \  <td class=\"diff-line-code\"></td>\n\
+            \</tr>"
+            tbody
+          return tbody
 
 
 -- getElementsByClassName :: forall eff. String -> b -> Eff (dom :: DOM | eff) (Array HTMLElement)
