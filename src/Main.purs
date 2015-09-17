33d3228a9ea548dbdefa6c690489d1ee2316de6c
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-09-17 22:02:03 +0900

    adapt new GitHub suppressing style

--- src/Main.purs
+++ src/Main.purs
@@ -1,21 +1,17 @@
-module Main (main, expand) where
+module Main (main) where
 
-import Prelude (Unit (), unit, (++), bind, ($), return, (<$>), const)
+import Prelude (Unit (), unit, bind, ($), return)
 import DOM (DOM ())
 import Control.Monad.Eff (Eff ())
-import Control.Monad.Eff.Console (CONSOLE ())
 import qualified Data.Array (head, filterM, null) as A
 import Data.Foldable (traverse_)
 import Data.Maybe (Maybe (Just, Nothing))
-import qualified Data.String (drop) as S
-import Data.String.Regex (regex, noFlags, test, replace)
+import Data.String.Regex (regex, noFlags, test)
 import Data.DOM.Simple.Document (createElement)
-import Data.DOM.Simple.Element (getElementsByClassName, getElementById, getAttribute, setAttribute, setStyleAttr, setInnerHTML, children, appendChild)
-import Data.DOM.Simple.Events (addMouseEventListener, MouseEventType (MouseClickEvent))
-import Data.DOM.Simple.Types (DOMEvent ())
+import Data.DOM.Simple.Element (getElementsByClassName, getAttribute, setAttribute, setInnerHTML, appendChild, classAdd)
 import Data.DOM.Simple.Window (document, globalWindow)
 
-main :: Eff (dom :: DOM, console :: CONSOLE) Unit
+main :: Eff (dom :: DOM) Unit
 main = do
   doc <- document globalWindow
   fileDivs <- getElementsByClassName "file" doc
@@ -35,56 +31,21 @@ main = do
             Nothing -> return false
         else return false
     collapse doc div = do
-      tid <- S.drop 5 <$> getAttribute "id" div
-      tables <- getElementsByClassName "diff-table" div
-      case A.head tables of
-        Just table -> do
-          tbodies <- children table
-          case A.head tbodies of
-            Just tbody -> do
-              setAttribute "id" (origId tid) tbody
-              setStyleAttr "display" "none" tbody
+      dataDivs <- getElementsByClassName "data" div
+      if A.null dataDivs
+        then return unit
+        else
+          case A.head dataDivs of
+            Just dataDiv -> do
+              classAdd "suppressed" dataDiv
+              image <- createImageDiv doc
+              appendChild div image
             Nothing -> return unit
-          gcTbody <- createGcTbody tid doc
-          appendChild table gcTbody
-          ma <- getElementById (expandId tid) doc
-          case ma of
-            Just a -> addMouseEventListener MouseClickEvent (const (expand tid) :: forall e. DOMEvent -> Eff (dom :: DOM | e) Unit) a
-            Nothing -> return unit
-        Nothing -> return unit
       where
-        createGcTbody tid doc = do
-          tbody <- createElement "tbody" doc
-          setAttribute "id" (replId tid) tbody
-          let template = "<tr class=\"js-expandable-line\" data-position=\"0\">\n\
-                         \  <td class=\"blob-num blob-num-expandable\" style=\"width: 99px;\">\n\
-                         \    <a id=\"{id}\" class=\"diff-expander\" title=\"Expand\" aria-label=\"Expand\">\n\
-                         \      <span class=\"octicon octicon-unfold\"></span>\n\
-                         \    </a>\n\
-                         \  </td>\n\
-                         \  <td class=\"blob-code blob-code-hunk\"></td>\n\
-                         \  </tr>\n\
-                         \</tr>"
-              pattern = regex "\\{id\\}" noFlags
+        createImageDiv doc = do
+          div <- createElement "div" doc
+          setAttribute "class" "image" div
           setInnerHTML
-            (replace pattern (expandId tid) template)
-            tbody
-          return tbody
-
-origId = ("orig-" ++)
-replId = ("repl-" ++)
-expandId = ("expand-" ++)
-
-expand :: String -> Eff (dom :: DOM) Unit
-expand tid = do
-  doc <- document globalWindow
-  mOrigTBody <- getElementById (origId tid) doc
-  mReplTBody <- getElementById (replId tid) doc
-  case mOrigTBody of
-    Just origTBody ->
-      case mReplTBody of
-        Just replTBody -> do
-          setStyleAttr "display" "none" replTBody
-          setStyleAttr "display" "block" origTBody
-        Nothing -> return unit
-    Nothing -> return unit
+            "<a href=\"#\" class=\"js-details-target\">Diff suppressed. Click to show.</a>"
+            div
+          return div
